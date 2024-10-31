f32d7ba173eab3c92cc0a46d30eec986bc8297b0
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-10-31 18:03:25 -0400

    project

---
+++ vite-project/JS/products.js
@@ -0,0 +1,28 @@
+products = [
+    {
+        name: "Clorox",
+        price: 16000000,
+        description: "Cleans stains like never before! Abraham Lincoln touched this."
+    },
+    {
+        name: "Wet Wipes",
+        price: 13000000,
+        description: "Can wipe anything, even your family and friends! Use cautiously."
+    },
+    {
+        name: "Mop",
+        price: 500000000,
+        description: "A dead body. You can mop the floor with this and it'll leave your floor with a breathtaking marble design!"
+    },
+    {
+        name: "Vaccum",
+        price: 1000000000,
+        description: "A high quality vaccum that can even suck up dead bodies! Perfect for any inconvenience."
+    },
+    {
+        name: "Broom",
+        price: 4600000000,
+        description: "Sweeps your eyes away in a matter of seconds! This broom is so fast that it may even sweep your house away! Use cautiously."
+    },
+    {}
+]
