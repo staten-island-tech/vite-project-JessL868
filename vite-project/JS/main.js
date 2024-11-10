b679e8fb2ac925120dac786081c144d9541e723e
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-11-09 19:27:54 -0500

    insert imgs

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -18,3 +18,81 @@ products.forEach((product) => DOMSelectors.container.insertAdjacentHTML("beforee
  //movies.sort
  //movies.includes
  //movies.filter((movie) => movie.actors.includes("Liam"))
+
+document.querySelector("#btn").addEventListener("click", function() {
+    if(document.body.classList.contains("light")){
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+    } else {
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    }
+
+});
+document.querySelector("#all").addEventListener("click", function() {
+    DOMSelectors.container.innerHTML = '';
+    products.forEach((product) => DOMSelectors.container.insertAdjacentHTML("beforeend", 
+        `<div class="card">
+            <h2 class = "name">${product.name}</h2>
+            <h2 class = "price">${product.price}</h2>
+            <img id = "card-img" src="${product.img}" alt="">
+        </div>`
+    ));
+});
+
+document.querySelector("#electronics").addEventListener("click", function() {
+    DOMSelectors.container.innerHTML = ''; 
+
+    const electronicsProducts = products.filter((product) => product.category === "Electronics");
+
+    electronicsProducts.forEach((product) => {DOMSelectors.container.insertAdjacentHTML("beforeend", `
+            <div class="card">
+                <h2 class="name">${product.name}</h2>
+                <h2 class="price">${product.price}</h2>
+                <img id="card-img" src="${product.img}" alt="">
+            </div>
+        `);
+    });
+});
+
+document.querySelector("#home").addEventListener("click", function () {
+    DOMSelectors.container.innerHTML = '';
+
+    const homeProducts = products.filter((product) => product.category === "Home");
+
+    homeProducts.forEach((product) => {DOMSelectors.container.insertAdjacentHTML("beforeend", 
+        `<div class="card">
+            <h2 class="name">${product.name}</h2>
+            <h2 class="price">${product.price}</h2>
+            <img id="card-img" src="${product.img}" alt="">
+        </div>`);
+    });
+});
+
+document.querySelector("#candy").addEventListener("click", function () {
+    DOMSelectors.container.innerHTML = '';
+
+    const candyProducts = products.filter((product) => product.category === "Candy");
+
+    candyProducts.forEach((product) => {DOMSelectors.container.insertAdjacentHTML("beforeend", 
+        `<div class="card">
+            <h2 class="name">${product.name}</h2>
+            <h2 class="price">${product.price}</h2>
+            <img id="card-img" src="${product.img}" alt="">
+        </div>`);
+    });
+});
+document.querySelector("#cleaning").addEventListener("click", function () {
+    DOMSelectors.container.innerHTML = '';
+
+    const cleaningProducts = products.filter((product) => product.category === "Cleaning");
+
+    cleaningProducts.forEach((product) => {DOMSelectors.container.insertAdjacentHTML("beforeend", 
+        `<div class="card">
+            <h2 class="name">${product.name}</h2>
+            <h2 class="price">${product.price}</h2>
+            <img id="card-img" src="${product.img}" alt="">
+        </div>`);
+    });
+});
+
--- vite-project/JS/products.js
+++ vite-project/JS/products.js
@@ -2,68 +2,124 @@ const products = [
     {
         name: "Clorox",
         price: "$16000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Cleaning"
     },
     {
         name: "Wet Wipes",
         price: "$13000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Cleaning"
     },
     {
         name: "Mop",
         price: "$500000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FAmerican-Market-Cotton-Solid-Handle%2Fdp%2FB06VXV3VPX&psig=AOvVaw3tyD6eafbhgJT6D351m_7o&ust=1731005261767000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCqxrfyyIkDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Cleaning"
     },
     {
         name: "Vaccum",
         price: "$1000000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Cleaning"
     },
     {
         name: "Broom",
         price: "$4600000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Cleaning"
     },
     {
         name: "Twix",
         price: "$10000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Candy"
     },
     {
         name: "Hershey's",
         price: "$12000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Candy"
     },
     {
         name: "Milky Way",
         price: "$11000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Candy"
     },
     {
         name: "M&M's",
         price: "$10500000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Candy"
     },
     {
         name: "Crunchy",
         price: "$10000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Candy"
     },
     {
         name: "Wall",
         price: "$1200000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Home"
     },
     {
         name: "Door",
         price: "$1050000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Home"
     },
     {
         name: "Floor",
         price: "$1400000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "",
+        category: "Home"
+    },
+    {
+        name: "Roof",
+        price: "$2000000000",
+        img: "",
+        category: "Home"
+    },
+    {
+        name: "Window",
+        price: "$500000000",
+        img: "",
+        category: "Home"
+    },
+    {
+        name: "Smartphone",
+        price: "$800000000",
+        img: "",
+        category: "Electronics"
+    },
+    {
+        name: "Laptop",
+        price: "$1200000000",
+        img: "",
+        category: "Electronics"
+    },
+    {
+        name: "Smartwatch",
+        price: "$250000000",
+        img: "",
+        category: "Electronics"
+    },
+    {
+        name: "Wireless Headphones",
+        price: "$300000000",
+        img: "",
+        category: "Electronics"
+    },
+    {
+        name: "Smart TV",
+        price: "$1600000000",
+        img: "",
+        category: "Electronics"
     }
 ];
+
 
 export {products}
