22d6526624dd1d1175b4fec3a44615dc7598390a
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-11-12 18:35:44 -0500

    moved imgs

--- vite-project/JS/products.js
+++ vite-project/JS/products.js
@@ -2,121 +2,121 @@ const products = [
     {
         name: "Clorox",
         price: "$16000000",
-        img: "https://pbs.twimg.com/profile_images/1179118703327076352/oeXb4f39_400x400.jpg",
+        img: "images/clorox.jpg",
         category: "Cleaning"
     },
     {
         name: "Wet Wipes",
         price: "$13000000",
-        img: "https://target.scene7.com/is/image/Target/GUEST_f027e7f1-508a-428e-8386-2ef37a88d7ab?wid=488&hei=488&fmt=pjpeg",
+        img: "images/wet_ones.avif",
         category: "Cleaning"
     },
     {
         name: "Mop",
         price: "$500000000",
-        img: "https://fuller.com/cdn/shop/products/wet-mop-complete-absorbent-quality-cotton-yarn-floor-cleaner-w806-handle-mops_1200x1200.jpg?v=1596013754",
+        img: "images/mop.webp",
         category: "Cleaning"
     },
     {
         name: "Vaccum",
         price: "$1000000000",
-        img: "https://www.kroger.com/product/images/large/front/0001112025908",
+        img: "images/vaccum.webp",
         category: "Cleaning"
     },
     {
         name: "Broom",
         price: "$4600000000",
-        img: "https://m.media-amazon.com/images/I/71J2RnVx7uL.jpg",
+        img: "images/broom.jpg",
         category: "Cleaning"
     },
     {
         name: "Twix",
         price: "$10000000",
-        img: "https://images.heb.com/is/image/HEBGrocery/001391957-1",
+        img: "images/twix.webp",
         category: "Candy"
     },
     {
         name: "Hershey's",
         price: "$12000000",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrdc2qF-j24RVzA6i7HYVmBl83Gyfl7tq7Ew&s",
+        img: "images/hersheys.jfif",
         category: "Candy"
     },
     {
         name: "Milky Way",
         price: "$11000000",
-        img: "https://images.albertsons-media.com/is/image/ABS/960079440-C1N1?$ng-ecom-pdp-mobile$&defaultImage=Not_Available",
+        img: "images/milky_way.webp",
         category: "Candy"
     },
     {
         name: "M&M's",
         price: "$10500000",
-        img: "https://s7d6.scene7.com/is/image/bjs/27278",
+        img: "images/m&ms.avif",
         category: "Candy"
     },
     {
         name: "Crunch",
         price: "$10000000",
-        img: "https://allcitycandy.com/cdn/shop/products/Default_00099900908363_CF_44012576_2048x.jpg?v=1633467115",
+        img: "images/crunch.webp",
         category: "Candy"
     },
     {
         name: "Wall",
         price: "$1200000000",
-        img: "https://img.freepik.com/free-photo/empty-room-gray-wall-room-with-wooden-floor_53876-128781.jpg",
+        img: "images/wall.avif",
         category: "Home"
     },
     {
         name: "Door",
         price: "$1050000000",
-        img: "https://realcraft.com/cdn/shop/products/ShakerDoubleFlatPanelMahogany-CenterBar_d3c3b163-99e9-4963-8e2b-cf64c1d41c29_1200x.jpg?v=1722436613",
+        img: "images/door.webp",
         category: "Home"
     },
     {
         name: "Floor",
         price: "$1400000000",
-        img: "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2023/05/solid-oak-flooring.jpeg-1-e1684409736918.jpg",
+        img: "images/floor.webp",
         category: "Home"
     },
     {
         name: "Roof",
         price: "$2000000000",
-        img: "https://contactcolonial.com/wp-content/uploads/2024/01/concrete-tile-roofing.jpg",
+        img: "images/roof.jpg",
         category: "Home"
     },
     {
         name: "Window",
         price: "$500000000",
-        img: "https://images.thdstatic.com/productImages/d6e12e93-6d27-4495-909e-f9cd24bd928e/svn/american-craftsman-single-hung-windows-50-sh-fin-64_300.jpg",
+        img: "images/window.avif",
         category: "Home"
     },
     {
         name: "Smartphone",
         price: "$800000000",
-        img: "https://m.media-amazon.com/images/I/61L5R2S1vlL.jpg",
+        img: "images/iphone.jpg",
         category: "Electronics"
     },
     {
         name: "Laptop",
         price: "$1200000000",
-        img: "https://m.media-amazon.com/images/I/711jgF2LHPL._AC_UF894,1000_QL80_.jpg",
+        img: "images/laptop.jpg",
         category: "Electronics"
     },
     {
         name: "Smartwatch",
         price: "$250000000",
-        img: "https://m.media-amazon.com/images/I/71pBtMd2vjL.jpg",
+        img: "images/smartwatch.jpg",
         category: "Electronics"
     },
     {
         name: "Wireless Headphones",
         price: "$300000000",
-        img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQTR3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1687660671097",
+        img: "images/headphones.jfif",
         category: "Electronics"
     },
     {
         name: "Smart TV",
         price: "$1600000000",
-        img: "https://www.badcock.com/images/thumbs/0040515_tcl-40-android-smart-led-tv.jpeg",
+        img: "images/smart_tv.jpeg",
         category: "Electronics"
     }
 ];
