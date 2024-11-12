97d02a297076d80fc6bcd5a31103b81285b8abe5
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-11-12 13:28:34 -0500

    forgot to commit :cry:

--- vite-project/JS/products.js
+++ vite-project/JS/products.js
@@ -2,68 +2,124 @@ const products = [
     {
         name: "Clorox",
         price: "$16000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://pbs.twimg.com/profile_images/1179118703327076352/oeXb4f39_400x400.jpg",
+        category: "Cleaning"
     },
     {
         name: "Wet Wipes",
         price: "$13000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://target.scene7.com/is/image/Target/GUEST_f027e7f1-508a-428e-8386-2ef37a88d7ab?wid=488&hei=488&fmt=pjpeg",
+        category: "Cleaning"
     },
     {
         name: "Mop",
         price: "$500000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FAmerican-Market-Cotton-Solid-Handle%2Fdp%2FB06VXV3VPX&psig=AOvVaw3tyD6eafbhgJT6D351m_7o&ust=1731005261767000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCqxrfyyIkDFQAAAAAdAAAAABAE"
+        img: "https://fuller.com/cdn/shop/products/wet-mop-complete-absorbent-quality-cotton-yarn-floor-cleaner-w806-handle-mops_1200x1200.jpg?v=1596013754",
+        category: "Cleaning"
     },
     {
         name: "Vaccum",
         price: "$1000000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://www.kroger.com/product/images/large/front/0001112025908",
+        category: "Cleaning"
     },
     {
         name: "Broom",
         price: "$4600000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://m.media-amazon.com/images/I/71J2RnVx7uL.jpg",
+        category: "Cleaning"
     },
     {
         name: "Twix",
         price: "$10000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://images.heb.com/is/image/HEBGrocery/001391957-1",
+        category: "Candy"
     },
     {
         name: "Hershey's",
         price: "$12000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrdc2qF-j24RVzA6i7HYVmBl83Gyfl7tq7Ew&s",
+        category: "Candy"
     },
     {
         name: "Milky Way",
         price: "$11000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://images.albertsons-media.com/is/image/ABS/960079440-C1N1?$ng-ecom-pdp-mobile$&defaultImage=Not_Available",
+        category: "Candy"
     },
     {
         name: "M&M's",
         price: "$10500000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://s7d6.scene7.com/is/image/bjs/27278",
+        category: "Candy"
     },
     {
-        name: "Crunchy",
+        name: "Crunch",
         price: "$10000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://allcitycandy.com/cdn/shop/products/Default_00099900908363_CF_44012576_2048x.jpg?v=1633467115",
+        category: "Candy"
     },
     {
         name: "Wall",
         price: "$1200000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://img.freepik.com/free-photo/empty-room-gray-wall-room-with-wooden-floor_53876-128781.jpg",
+        category: "Home"
     },
     {
         name: "Door",
         price: "$1050000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://realcraft.com/cdn/shop/products/ShakerDoubleFlatPanelMahogany-CenterBar_d3c3b163-99e9-4963-8e2b-cf64c1d41c29_1200x.jpg?v=1722436613",
+        category: "Home"
     },
     {
         name: "Floor",
         price: "$1400000000",
-        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcircle-colorful-icons-representing-technology-business-communication_298696561.htm&psig=AOvVaw2bNP4pxuKnoj34A_sr7AJY&ust=1731092008192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj83ozyyokDFQAAAAAdAAAAABAE"
+        img: "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2023/05/solid-oak-flooring.jpeg-1-e1684409736918.jpg",
+        category: "Home"
+    },
+    {
+        name: "Roof",
+        price: "$2000000000",
+        img: "https://contactcolonial.com/wp-content/uploads/2024/01/concrete-tile-roofing.jpg",
+        category: "Home"
+    },
+    {
+        name: "Window",
+        price: "$500000000",
+        img: "https://images.thdstatic.com/productImages/d6e12e93-6d27-4495-909e-f9cd24bd928e/svn/american-craftsman-single-hung-windows-50-sh-fin-64_300.jpg",
+        category: "Home"
+    },
+    {
+        name: "Smartphone",
+        price: "$800000000",
+        img: "https://m.media-amazon.com/images/I/61L5R2S1vlL.jpg",
+        category: "Electronics"
+    },
+    {
+        name: "Laptop",
+        price: "$1200000000",
+        img: "https://m.media-amazon.com/images/I/711jgF2LHPL._AC_UF894,1000_QL80_.jpg",
+        category: "Electronics"
+    },
+    {
+        name: "Smartwatch",
+        price: "$250000000",
+        img: "https://m.media-amazon.com/images/I/71pBtMd2vjL.jpg",
+        category: "Electronics"
+    },
+    {
+        name: "Wireless Headphones",
+        price: "$300000000",
+        img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQTR3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1687660671097",
+        category: "Electronics"
+    },
+    {
+        name: "Smart TV",
+        price: "$1600000000",
+        img: "https://www.badcock.com/images/thumbs/0040515_tcl-40-android-smart-led-tv.jpeg",
+        category: "Electronics"
     }
 ];
+
 
 export {products}
